fcea6a681de99a6a20809b6a0159f4ce24a0eeb0
Piotr Rajtar <piotrek.rajtar@gmail.com>
2020-10-06 20:52:34 +0200

    Make stylistic changes for heder

--- src/components/layout/Header/Header.js
+++ src/components/layout/Header/Header.js
@@ -9,8 +9,6 @@ import styles from './Header.module.scss';
 import { ShopLogo } from '../../features/ShopLogo/ShopLogo';
 import { CartLogo } from '../../features/CartLogo/CartLogo';
 
-
-
 const Component = () => (
   <header className={styles.root}>
     <ShopLogo />
