7ce9ac4be8ac86eef7ff333075dc92dfb0370fab
Piotr Rajtar <piotrek.rajtar@gmail.com>
2020-10-06 21:30:31 +0200

    Add Footer view

--- src/components/layout/Footer/Footer.js
+++ src/components/layout/Footer/Footer.js
@@ -1,24 +1,42 @@
 import React from 'react';
-import PropTypes from 'prop-types';
-
-import clsx from 'clsx';
+// import PropTypes from 'prop-types';
 
 // import { connect } from 'react-redux';
 // import { reduxSelector, reduxActionCreator } from '../../../redux/exampleRedux.js';
 
 import styles from './Footer.module.scss';
 
-const Component = ({className, children}) => (
-  <div className={clsx(className, styles.root)}>
-    <h2>Footer</h2>
-    {children}
+import FacebookIcon from '@material-ui/icons/Facebook';
+import InstagramIcon from '@material-ui/icons/Instagram';
+import YouTubeIcon from '@material-ui/icons/YouTube';
+import TwitterIcon from '@material-ui/icons/Twitter';
+
+const Component = () => (
+  <div className={styles.root}>
+
+    <a className={styles.link} href='https://www.facebook.com/'>
+      <FacebookIcon className={styles.icon} />
+    </a>
+
+    <a className={styles.link} href='https://www.instagram.com/'>
+      <InstagramIcon className={styles.icon} />
+    </a>
+
+    <a className={styles.link} href='https://www.youtube.com/'>
+      <YouTubeIcon className={styles.icon} />
+    </a>
+
+    <a className={styles.link} href='https://twitter.com/'>
+      <TwitterIcon className={styles.icon} />
+    </a>
+
   </div>
 );
 
-Component.propTypes = {
-  children: PropTypes.node,
-  className: PropTypes.string,
-};
+// Component.propTypes = {
+//   children: PropTypes.node,
+//   className: PropTypes.string,
+// };
 
 // const mapStateToProps = state => ({
 //   someProp: reduxSelector(state),
