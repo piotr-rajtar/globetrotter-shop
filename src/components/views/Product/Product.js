7a4fa92cd6ee5fac2e903406cacaa1e065642695
Piotr Rajtar <piotrek.rajtar@gmail.com>
2020-10-08 21:54:04 +0200

    Add Product pre-view

--- src/components/views/Product/Product.js
+++ src/components/views/Product/Product.js
@@ -1,37 +1,70 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import clsx from 'clsx';
+import { connect } from 'react-redux';
+import { getProductById } from '../../../redux/productsRedux';
 
-// import { connect } from 'react-redux';
-// import { reduxSelector, reduxActionCreator } from '../../../redux/exampleRedux.js';
+import { PhotoGallery } from '../../features/PhotoGallery/PhotoGallery';
+import { ProductCounter } from '../../features/ProductCounter/ProductCounter';
 
 import styles from './Product.module.scss';
 
-const Component = ({className, children}) => (
-  <div className={clsx(className, styles.root)}>
-    <h2>Product</h2>
-    {children}
-  </div>
-);
+import Paper from '@material-ui/core/Paper';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
 
-Component.propTypes = {
-  children: PropTypes.node,
-  className: PropTypes.string,
-};
 
-// const mapStateToProps = state => ({
-//   someProp: reduxSelector(state),
-// });
+class Component extends React.Component {
+  static propTypes = {
+    product: PropTypes.object,
+  }
+
+  render() {
+    const { product } = this.props;
+    console.log(product);
+    return(
+      <Paper>
+        <Card>
+          <CardContent>
+            <Typography>
+              {product.name}
+            </Typography>
+            <Typography>
+              {product.description}
+            </Typography>
+
+            <PhotoGallery images={product.photo} />
+
+            <ProductCounter />
+
+            <Typography>
+              Price: {product.price}
+            </Typography>
+          </CardContent>
+          <CardActions>
+            <Button>Add to cart</Button>
+          </CardActions>
+        </Card>
+      </Paper>
+    );
+  }
+}
+
+const mapStateToProps = (state, props) => ({
+  product: getProductById(state, props.match.params.id),
+});
 
 // const mapDispatchToProps = dispatch => ({
 //   someAction: arg => dispatch(reduxActionCreator(arg)),
 // });
 
-// const Container = connect(mapStateToProps, mapDispatchToProps)(Component);
+const Container = connect(mapStateToProps)(Component);
 
 export {
-  Component as Product,
-  // Container as Product,
+  // Component as Product,
+  Container as Product,
   Component as ProductComponent,
 };
--- src/components/views/Product/Product.test.js
+++ src/components/views/Product/Product.test.js
@@ -2,9 +2,18 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import { ProductComponent } from './Product';
 
+const product = {
+  id: '1',
+  name: 'name',
+  type: 'type',
+  price: 1,
+  photo: ['image'],
+  description: 'description',
+};
+
 describe('Component Product', () => {
   it('should render without crashing', () => {
-    const component = shallow(<ProductComponent />);
+    const component = shallow(<ProductComponent product={product} />);
     expect(component).toBeTruthy();
   });
 });
