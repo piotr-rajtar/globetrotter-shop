bba652f3ce1d8732066025bf23488120936bae67
Piotr Rajtar <piotrek.rajtar@gmail.com>
2020-10-10 20:17:43 +0200

    Add ShoppingCart view

--- src/components/views/ShoppingCart/ShoppingCart.js
+++ src/components/views/ShoppingCart/ShoppingCart.js
@@ -1,37 +1,49 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import clsx from 'clsx';
 
-// import { connect } from 'react-redux';
-// import { reduxSelector, reduxActionCreator } from '../../../redux/exampleRedux.js';
+import { connect } from 'react-redux';
+import { getAllCartProducts } from '../../../redux/cartRedux';
+
+import { CartProductList } from '../../features/CartProductList/CartProductList';
 
 import styles from './ShoppingCart.module.scss';
 
-const Component = ({className, children}) => (
-  <div className={clsx(className, styles.root)}>
-    <h2>ShoppingCart</h2>
-    {children}
-  </div>
-);
+import Paper from '@material-ui/core/Paper';
+import Typography from '@material-ui/core/Typography';
 
-Component.propTypes = {
-  children: PropTypes.node,
-  className: PropTypes.string,
-};
+class Component extends React.Component {
 
-// const mapStateToProps = state => ({
-//   someProp: reduxSelector(state),
-// });
+  static propTypes = {
+    cartProducts: PropTypes.array,
+  }
+
+  render() {
+    const { cartProducts } = this.props;
+
+    return(
+      <Paper>
+        <Typography className={styles.title} gutterBottom variant="h3" component="h1">
+          Cart
+        </Typography>
+        <CartProductList cartProducts={cartProducts} />
+      </Paper>
+    );
+  }
+}
+
+const mapStateToProps = state => ({
+  cartProducts: getAllCartProducts(state),
+});
 
 // const mapDispatchToProps = dispatch => ({
 //   someAction: arg => dispatch(reduxActionCreator(arg)),
 // });
 
-// const Container = connect(mapStateToProps, mapDispatchToProps)(Component);
+const Container = connect(mapStateToProps)(Component);
 
 export {
-  Component as ShoppingCart,
-  // Container as ShoppingCart,
+  // Component as ShoppingCart,
+  Container as ShoppingCart,
   Component as ShoppingCartComponent,
 };
