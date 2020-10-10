c96dcc5c9790bc05091b1066f08fa57865a35c0e
Piotr Rajtar <piotrek.rajtar@gmail.com>
2020-10-10 19:37:18 +0200

    Add addCartProduct action into cartRedux file

--- src/redux/cartRedux.js
+++ src/redux/cartRedux.js
@@ -10,10 +10,14 @@ const FETCH_START = createActionName('FETCH_START');
 const FETCH_SUCCESS = createActionName('FETCH_SUCCESS');
 const FETCH_ERROR = createActionName('FETCH_ERROR');
 
+const ADD_CART_PRODUCT = createActionName('ADD_CART_PRODUCT');
+
 /* action creators */
 export const fetchStarted = payload => ({ payload, type: FETCH_START });
 export const fetchSuccess = payload => ({ payload, type: FETCH_SUCCESS });
 export const fetchError = payload => ({ payload, type: FETCH_ERROR });
+
+export const addCartProduct = payload => ({ payload, type: ADD_CART_PRODUCT});
 
 /* reducer */
 export const reducer = (statePart = [], action = {}) => {
@@ -44,6 +48,15 @@ export const reducer = (statePart = [], action = {}) => {
           active: false,
           error: action.payload,
         },
+      };
+    }
+    case ADD_CART_PRODUCT: {
+      return {
+        ...statePart,
+        data: [
+          ...statePart.data,
+          action.payload,
+        ],
       };
     }
     default:
