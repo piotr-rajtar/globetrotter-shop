c52374d8bee55dc01cb12a683a03b8f614f4a472
Piotr Rajtar <piotrek.rajtar@gmail.com>
2020-10-12 23:03:38 +0200

    Add redux for orders

---
+++ src/redux/ordersRedux.js
@@ -0,0 +1,64 @@
+/* selectors */
+
+/* action name creator */
+const reducerName = 'order';
+const createActionName = name => `app/${reducerName}/${name}`;
+
+/* action types */
+const FETCH_START = createActionName('FETCH_START');
+const FETCH_SUCCESS = createActionName('FETCH_SUCCESS');
+const FETCH_ERROR = createActionName('FETCH_ERROR');
+
+const ADD_ORDER = createActionName('ADD_ORDER');
+
+/* action creators */
+export const fetchStarted = payload => ({ payload, type: FETCH_START });
+export const fetchSuccess = payload => ({ payload, type: FETCH_SUCCESS });
+export const fetchError = payload => ({ payload, type: FETCH_ERROR });
+
+export const addOrder = payload => ({ payload, type: ADD_ORDER });
+
+/* reducer */
+export const reducer = (statePart = [], action = {}) => {
+  switch (action.type) {
+    case FETCH_START: {
+      return {
+        ...statePart,
+        loading: {
+          active: true,
+          error: false,
+        },
+      };
+    }
+    case FETCH_SUCCESS: {
+      return {
+        ...statePart,
+        loading: {
+          active: false,
+          error: false,
+        },
+        data: action.payload,
+      };
+    }
+    case FETCH_ERROR: {
+      return {
+        ...statePart,
+        loading: {
+          active: false,
+          error: action.payload,
+        },
+      };
+    }
+    case ADD_ORDER: {
+      return {
+        ...statePart,
+        data: [
+          ...statePart.data,
+          action.payload,
+        ],
+      };
+    }
+    default:
+      return statePart;
+  }
+};
