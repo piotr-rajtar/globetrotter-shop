485ee19cb30b9a6b80675948958405bfc0c0d8a5
Piotr Rajtar <piotrek.rajtar@gmail.com>
2020-10-12 23:12:26 +0200

    Add OrderSummary component view

--- src/components/views/OrderSummary/OrderSummary.js
+++ src/components/views/OrderSummary/OrderSummary.js
@@ -1,37 +1,150 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import clsx from 'clsx';
+import { OrderSummaryList } from '../../features/OrderSummaryList/OrderSummaryList';
+import { OrderForm } from '../../features/OrderForm/OrderForm';
 
-// import { connect } from 'react-redux';
-// import { reduxSelector, reduxActionCreator } from '../../../redux/exampleRedux.js';
+import { connect } from 'react-redux';
+import { getAllCartProducts } from '../../../redux/cartRedux';
+import { addOrder } from '../../../redux/ordersRedux';
+import { createDate } from '../../../utils';
 
 import styles from './OrderSummary.module.scss';
 
-const Component = ({className, children}) => (
-  <div className={clsx(className, styles.root)}>
-    <h2>OrderSummary</h2>
-    {children}
-  </div>
-);
+import Paper from '@material-ui/core/Paper';
+import Typography from '@material-ui/core/Typography';
 
-Component.propTypes = {
-  children: PropTypes.node,
-  className: PropTypes.string,
-};
+class Component extends React.Component {
 
-// const mapStateToProps = state => ({
-//   someProp: reduxSelector(state),
-// });
+  state = {
+    orderData: {
+      name: '',
+      surname: '',
+      email: '',
+      telephone: '',
+      ordered: '',
+      orderDetails: this.props.cartProducts,
+    },
+  }
 
-// const mapDispatchToProps = dispatch => ({
-//   someAction: arg => dispatch(reduxActionCreator(arg)),
-// });
+  static propTypes = {
+    cartProducts: PropTypes.array,
+    addOrder: PropTypes.func,
+  }
 
-// const Container = connect(mapStateToProps, mapDispatchToProps)(Component);
+  totalCost() {
+    const { cartProducts } = this.props;
+
+    let totalCost = 0;
+
+    cartProducts.map(cartProduct => totalCost += cartProduct.finalPrice);
+
+    return totalCost;
+  }
+
+  handleChange = (event) => {
+    const { orderData } = this.state;
+    const { value, id } = event.target;
+
+    this.setState({
+      orderData: {
+        ...orderData,
+        [id]: value,
+      },
+    });
+  }
+
+  setOrderDate = () => {
+    const { orderData } = this.state;
+    const date = createDate();
+
+    this.setState({
+      orderData: {
+        ...orderData,
+        ordered: date,
+      },
+    });
+  }
+
+  clearCart() {
+    const { orderData } = this.state;
+
+    this.setState({
+      orderData: {
+        name: '',
+        surname: '',
+        email: '',
+        telephone: '',
+        ordered: '',
+      },
+    });
+
+    console.log(orderData);
+  }
+
+  submitForm = (event) => {
+    const { orderData } = this.state;
+    const { addOrder } = this.props;
+
+    event.preventDefault();
+
+    let error = null;
+
+    if(!orderData.name.length || !orderData.surname.length || !orderData.email.length || !orderData.telephone.length) error='All form fields should be filled';
+    else if(orderData.name.length > 15 || orderData.surname.length > 20) error ='Name or surname is too long. Name max 15 characters, surname max 20';
+
+    if(!error) {
+      addOrder(orderData);
+      alert('Order submitted successfully');
+      this.setState({
+        orderData: {
+          name: '',
+          surname: '',
+          email: '',
+          telephone: '',
+          ordered: '',
+        },
+      });
+    } else {
+      alert(error);
+    }
+  }
+
+  render() {
+    const { cartProducts } = this.props;
+    const { orderData } = this.state;
+
+    return(
+      <Paper>
+        <OrderSummaryList cartProducts={cartProducts} />
+
+        <Typography className={styles.title} gutterBottom variant="h4" component="h1">
+          Total cost:{this.totalCost()}$
+        </Typography>
+
+        <OrderForm
+          orderData={orderData}
+          handleChange={this.handleChange}
+          submitForm={this.submitForm}
+          setOrderDate={this.setOrderDate}
+        />
+      </Paper>
+    );
+  }
+}
+
+const mapStateToProps = state => ({
+  cartProducts: getAllCartProducts(state),
+});
+
+const mapDispatchToProps = dispatch => ({
+  addOrder: orderData => dispatch(addOrder(orderData)),
+});
+
+const Container = connect(mapStateToProps, mapDispatchToProps)(Component);
 
 export {
-  Component as OrderSummary,
-  // Container as OrderSummary,
+  // Component as OrderSummary,
+  Container as OrderSummary,
   Component as OrderSummaryComponent,
 };
--- src/components/views/OrderSummary/OrderSummary.test.js
+++ src/components/views/OrderSummary/OrderSummary.test.js
@@ -2,9 +2,11 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import { OrderSummaryComponent } from './OrderSummary';
 
+const cartProducts = [];
+
 describe('Component OrderSummary', () => {
   it('should render without crashing', () => {
-    const component = shallow(<OrderSummaryComponent />);
+    const component = shallow(<OrderSummaryComponent cartProducts={cartProducts} />);
     expect(component).toBeTruthy();
   });
 });
