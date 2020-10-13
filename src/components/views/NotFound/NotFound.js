14f630eb86a8ebf4fc5017886849fc62d73266ee
Piotr Rajtar <piotrek.rajtar@gmail.com>
2020-10-13 21:15:53 +0200

    Add NotFound view

--- src/components/views/NotFound/NotFound.js
+++ src/components/views/NotFound/NotFound.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { NavLink } from 'react-router-dom';
 
 import clsx from 'clsx';
 
@@ -8,16 +9,33 @@ import clsx from 'clsx';
 
 import styles from './NotFound.module.scss';
 
-const Component = ({className, children}) => (
-  <div className={clsx(className, styles.root)}>
-    <h2>NotFound</h2>
-    {children}
-  </div>
+import Paper from '@material-ui/core/Paper';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+
+const Component = () => (
+  <Paper>
+    <img
+      src='https://images.unsplash.com/photo-1553981956-2335cffb5e50?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80'
+      alt='compass'
+      className={styles.image}
+    />
+    <Typography>Are you lost, Globetrotter?</Typography>
+    <Typography>Given page does not exist or has been removed already!</Typography>
+    <Button
+      variant="outlined"
+      color="primary"
+      size="large"
+      className={styles.button}
+      component={NavLink}
+      exact to={`/`}
+    >
+          GO BACK TO HOMEPAGE
+    </Button>
+  </Paper>
 );
 
 Component.propTypes = {
-  children: PropTypes.node,
-  className: PropTypes.string,
 };
 
 // const mapStateToProps = state => ({
