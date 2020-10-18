e818ec7ce13570889528c08f381dc9fe09b41b2b
Piotr Rajtar <piotrek.rajtar@gmail.com>
2020-10-18 14:14:49 +0200

    Add controllers for API product query handling

---
+++ controllers/product.controller.js
@@ -0,0 +1,34 @@
+const Product = require('../models/product.model');
+
+exports.getAllProducts = async (req, res) => {
+    try {
+        const result = await Product
+        .find()
+        .select('name price photo')
+        .sort({ type: 1 });
+
+        if(!result) res.status(404).json({ message: 'Product not found' });
+        else {
+            res.json(result);
+        }  
+    }
+    catch(err) {
+        res.status(500).json(err);
+    }
+};
+
+exports.getProductById = async (req, res) => {
+    try {
+        const result = await Product.findById(req.params.id);
+        const arrayResult = [];
+
+        if(!result) res.status(404).json({ message: 'Product not found' });
+        else {
+            arrayResult.push(result);
+            res.json(arrayResult);
+        }
+    }
+    catch{
+        res.status(500).json(err);
+    }
+};
