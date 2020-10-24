bca36ab4d68858760bc3c6e3bc49ccca277663e6
Piotr Rajtar <piotrek.rajtar@gmail.com>
2020-10-24 19:59:09 +0200

    Clean code for Header

--- client/src/components/layout/Header/Header.js
+++ client/src/components/layout/Header/Header.js
@@ -1,8 +1,4 @@
 import React from 'react';
-// import PropTypes from 'prop-types';
-
-// import { connect } from 'react-redux';
-// import { reduxSelector, reduxActionCreator } from '../../../redux/exampleRedux.js';
 
 import styles from './Header.module.scss';
 
@@ -16,23 +12,7 @@ const Component = () => (
   </header>
 );
 
-// Component.propTypes = {
-//   children: PropTypes.node,
-//   className: PropTypes.string,
-// };
-
-// const mapStateToProps = state => ({
-//   someProp: reduxSelector(state),
-// });
-
-// const mapDispatchToProps = dispatch => ({
-//   someAction: arg => dispatch(reduxActionCreator(arg)),
-// });
-
-// const Container = connect(mapStateToProps, mapDispatchToProps)(Component);
-
 export {
   Component as Header,
-  // Container as Header,
   Component as HeaderComponent,
 };
