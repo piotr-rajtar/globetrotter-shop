103494abc9875f37592c47292af5d865fc85b4d1
Piotr Rajtar <piotrek.rajtar@gmail.com>
2020-10-24 19:56:20 +0200

    Clean code for Footer

--- client/src/components/layout/Footer/Footer.js
+++ client/src/components/layout/Footer/Footer.js
@@ -1,8 +1,4 @@
 import React from 'react';
-// import PropTypes from 'prop-types';
-
-// import { connect } from 'react-redux';
-// import { reduxSelector, reduxActionCreator } from '../../../redux/exampleRedux.js';
 
 import styles from './Footer.module.scss';
 
@@ -33,23 +29,7 @@ const Component = () => (
   </div>
 );
 
-// Component.propTypes = {
-//   children: PropTypes.node,
-//   className: PropTypes.string,
-// };
-
-// const mapStateToProps = state => ({
-//   someProp: reduxSelector(state),
-// });
-
-// const mapDispatchToProps = dispatch => ({
-//   someAction: arg => dispatch(reduxActionCreator(arg)),
-// });
-
-// const Container = connect(mapStateToProps, mapDispatchToProps)(Component);
-
 export {
   Component as Footer,
-  // Container as Footer,
   Component as FooterComponent,
 };
