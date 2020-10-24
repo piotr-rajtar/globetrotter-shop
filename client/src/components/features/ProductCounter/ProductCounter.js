19f9743575a4d7cc854c681f46478c2b13c80650
Piotr Rajtar <piotrek.rajtar@gmail.com>
2020-10-24 19:50:05 +0200

    Clean code for ProductCounter component

--- client/src/components/features/ProductCounter/ProductCounter.js
+++ client/src/components/features/ProductCounter/ProductCounter.js
@@ -1,9 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-// import { connect } from 'react-redux';
-// import { reduxSelector, reduxActionCreator } from '../../../redux/exampleRedux.js';
-
 import styles from './ProductCounter.module.scss';
 
 import TextField from '@material-ui/core/TextField';
@@ -47,18 +44,7 @@ Component.propTypes = {
   quantity: PropTypes.number,
 };
 
-// const mapStateToProps = state => ({
-//   someProp: reduxSelector(state),
-// });
-
-// const mapDispatchToProps = dispatch => ({
-//   someAction: arg => dispatch(reduxActionCreator(arg)),
-// });
-
-// const Container = connect(mapStateToProps, mapDispatchToProps)(Component);
-
 export {
   Component as ProductCounter,
-  // Container as ProductCounter,
   Component as ProductCounterComponent,
 };
