5bc2d625aa7892e7d049fd12be6ed0313b1ae0ef
Piotr Rajtar <piotrek.rajtar@gmail.com>
2020-10-24 19:40:04 +0200

    Clean code for CartProductList component

--- client/src/components/features/CartProductList/CartProductList.js
+++ client/src/components/features/CartProductList/CartProductList.js
@@ -1,9 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-// import { connect } from 'react-redux';
-// import { reduxSelector, reduxActionCreator } from '../../../redux/exampleRedux.js';
-
 import { CartProduct } from '../CartProduct/CartProduct';
 
 import styles from './CartProductList.module.scss';
@@ -18,7 +15,6 @@ const Component = ({cartProducts}) => (
     justify='center'
     alignItems='center'
   >
-
     {cartProducts.map(cartProduct => (
       <Grid key={cartProduct.id} item className={styles.itemContainer}>
 
@@ -34,18 +30,7 @@ Component.propTypes = {
   cartProducts: PropTypes.array,
 };
 
-// const mapStateToProps = state => ({
-//   someProp: reduxSelector(state),
-// });
-
-// const mapDispatchToProps = dispatch => ({
-//   someAction: arg => dispatch(reduxActionCreator(arg)),
-// });
-
-// const Container = connect(mapStateToProps, mapDispatchToProps)(Component);
-
 export {
   Component as CartProductList,
-  // Container as CartProductList,
   Component as CartProductListComponent,
 };
