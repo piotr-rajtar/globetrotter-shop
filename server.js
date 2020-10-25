9908e7e0dcff7923a935814601cb3903090c68fc
Piotr Rajtar <piotrek.rajtar@gmail.com>
2020-10-25 20:30:44 +0100

    Add remote DB path in server.js file

--- server.js
+++ server.js
@@ -31,7 +31,10 @@ app.use('*', (req, res) => {
 });
 
 /* MONGOOSE */
-mongoose.connect('mongodb://localhost:27017/globetrotterShop', { useNewUrlParser: true, useUnifiedTopology: true });
+const dbURI = process.env.NODE_ENV ==='production'
+  ? `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.n3z6t.mongodb.net/globetrotterShop?retryWrites=true&w=majority`
+  : 'mongodb://localhost:27017/globetrotterShop';
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true });
 const db = mongoose.connection;
 db.once('open', () => {
   console.log('Successfully connected to the database');
