a2ce89069c8f45b9dead4495a6cebbaf97643ee3
Piotr Rajtar <piotrek.rajtar@gmail.com>
2020-10-26 16:07:18 +0100

    Add email phone and name parsers in front utils

--- client/src/utils.js
+++ client/src/utils.js
@@ -10,3 +10,21 @@ export function createDate () {
   const formatedDate = date.toString();
   return formatedDate;
 }
+
+export function emailParser (email) {
+  const emailPattern = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g;
+
+  return emailPattern.test(email);
+}
+
+export function phoneParser (phone) {
+  const phonePattern = /^\s*(?:\+?(\d{1,3}))?([-. (]*(\d{3})[-. )]*)?((\d{3})[-. ]*(\d{2,4})(?:[-.x ]*(\d+))?)\s*$/gm;
+
+  return phonePattern.test(phone);
+}
+
+export function nameParser (name) {
+  const namePattern = /^([a-zA-Z]+|[a-zA-Z]+\s{1}[a-zA-Z]{1,}|[a-zA-Z]+\s{1}[a-zA-Z]{3,}\s{1}[a-zA-Z]{1,})$/g;
+
+  return namePattern.test(name);
+}
